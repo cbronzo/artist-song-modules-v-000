43d01bb3c007d83c0680edb78c79f4475be919e1
Catherine Bronzo <cbronzo@mac.com>
2018-07-06 23:34:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  extend Paramable::InstanceMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
